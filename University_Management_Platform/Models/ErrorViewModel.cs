81049bd75829ef3bdfff14fd7122b2fcec55d1c8
David38mk <davidnaumovski38@gmail.com>
2025-12-17 20:09:21 +0100

    Initial Commint

---
+++ University_Management_Platform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace University_Management_Platform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
